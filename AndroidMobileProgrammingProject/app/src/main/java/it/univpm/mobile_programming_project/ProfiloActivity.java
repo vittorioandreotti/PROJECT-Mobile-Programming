46d6baebe54214884cda13517c7b71bff0ce53cd
Utente <bocci.matteo98@gmail.com>
2020-05-05 19:59:19 +0200

    profilo

--- AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/ProfiloActivity.java
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/ProfiloActivity.java
@@ -1,4 +1,14 @@
 package it.univpm.mobile_programming_project;
 
-public class ProfiloActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfiloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilo);
+    }
 }
