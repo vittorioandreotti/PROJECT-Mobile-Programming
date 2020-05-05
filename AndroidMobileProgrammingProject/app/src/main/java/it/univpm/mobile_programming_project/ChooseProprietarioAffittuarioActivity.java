ede70b31e9c9353c018f6e57ec0f95d2ea567751
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-05-05 17:48:56 +0200

    Creati layout per login, registrazione e scelta tra proprietario e affittuario.

---
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/ChooseProprietarioAffittuarioActivity.java
@@ -0,0 +1,14 @@
+package it.univpm.mobile_programming_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseProprietarioAffittuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_proprietario_affittuario);
+    }
+}
