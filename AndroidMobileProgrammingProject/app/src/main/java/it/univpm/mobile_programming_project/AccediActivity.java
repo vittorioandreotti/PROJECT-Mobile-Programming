76039b90782c77c8a4d4b60487bc46bbc84b0eef
Utente <bocci.matteo98@gmail.com>
2020-05-10 10:16:25 +0200

    tutto

--- AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/AccediActivity.java
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/AccediActivity.java
@@ -15,40 +15,11 @@ import it.univpm.mobile_programming_project.fragment.FragmentTornei;
 
 public class AccediActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNav;
-    private FragmentHome fragmentHome;
-    private FragmentSpese fragmentSpese;
-    private FragmentTornei fragmentTornei;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accedi);
-        fragmentHome=new FragmentHome();
-        fragmentSpese = new FragmentSpese();
-        fragmentTornei = new FragmentTornei();
-
-        bottomNav=findViewById(R.id.bottom_nav);
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment=null;
-                switch(item.getItemId()){
-                    case R.id.menu_home:
-                        selectedFragment=fragmentHome;
-                        break;
-                    case R.id.menu_spese:
-                        selectedFragment=fragmentSpese;
-                        break;
-                    case R.id.menu_tornei:
-                        selectedFragment=fragmentTornei;
-                        break;
-                }
-                if(selectedFragment!=null){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.scrollviewProfilo, selectedFragment).commit();
-                }
-                return true;
-            }
-        });
     }
 }
