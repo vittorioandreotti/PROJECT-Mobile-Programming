aa29681cdb4e67d402cd075b26319848f5bb14f3
Utente <bocci.matteo98@gmail.com>
2020-05-15 20:26:28 +0200

    stile e pagine codici casa

--- AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/RegistrazioneActivity.java
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/RegistrazioneActivity.java
@@ -9,6 +9,6 @@ public class RegistrazioneActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrazione);
+        setContentView(R.layout.fragment_registrazione);
     }
 }
