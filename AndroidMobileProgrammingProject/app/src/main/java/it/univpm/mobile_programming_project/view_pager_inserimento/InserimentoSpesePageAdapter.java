414db277024ba43f15f7794513c9d72536c46902
Utente <bocci.matteo98@gmail.com>
2020-06-25 12:03:13 +0200

    date picker

---
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/view_pager_inserimento/InserimentoSpesePageAdapter.java
@@ -0,0 +1,60 @@
+package it.univpm.mobile_programming_project.view_pager_inserimento;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+public class InserimentoSpesePageAdapter extends FragmentPagerAdapter {
+
+    private int numbOfTabs;
+
+    public InserimentoSpesePageAdapter(@NonNull FragmentManager fm, int numbOfTabs) {
+        super(fm);
+        this.numbOfTabs = numbOfTabs;
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new InserisciSpesaCondominioFragment();
+            case 1:
+                return new InserisciBolletteFragment();
+            case 2:
+                return new InserisciAffittoFragment();
+            case 3:
+                return new InserisciSpesaComuneFragment();
+            default: return null;
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return numbOfTabs;
+    }
+
+    /**
+     * Return a unique identifier for the item at the given position.
+     *
+     * <p>The default implementation returns the given position.
+     * Subclasses should override this method if the positions of items can change.</p>
+     *
+     * @param position Position within this adapter
+     * @return Unique identifier for the item at position
+     */
+    @Override
+    public long getItemId(int position) {
+        return POSITION_NONE;
+    }
+}
---
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/view_pager_inserimento/InserisciSpesaCondominioFragment.java
@@ -0,0 +1,96 @@
+package it.univpm.mobile_programming_project.view_pager_inserimento;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import it.univpm.mobile_programming_project.R;
+import it.univpm.mobile_programming_project.utils.picker.DatePickerFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InserisciSpesaCondominioFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InserisciSpesaCondominioFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    private TextInputEditText textInputEditText;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public InserisciSpesaCondominioFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SpeseCondominioFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static InserisciSpesaCondominioFragment newInstance(String param1, String param2) {
+        InserisciSpesaCondominioFragment fragment = new InserisciSpesaCondominioFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_inserisci_spesa_condominio, container, false);
+
+        textInputEditText = v.findViewById(R.id.txtDataSpesaCondominio);
+        textInputEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+            }
+        });
+        return v;
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment(this);
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        newFragment.show(fm, "datePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        textInputEditText.setText(String.format("%d/%d/%d", dayOfMonth, month, year));
+    }
+}
+
