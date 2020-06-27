92766a83c9224e4837cbf9c8d6ac04176387a404
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-06-27 12:32:10 +0200

    Inserimento codice di partecipazione ad una casa e corretta la creazione

--- AndroidMobileProgrammingProject/functions/functions/casa.js
+++ AndroidMobileProgrammingProject/functions/functions/casa.js
@@ -8,15 +8,63 @@ let db = admin.firestore();
 */
 exports.creaCasa = functions.https.onCall((data, context) => {
 
-    const text = data.text;
+    const uid = context.auth.uid;
+    let nome = data.nome;
+    let indirizzo = data.indirizzo;
 
-    console.log(text);
-    console.log(text.nomeCasa);
+    if(
+        nome == undefined || nome == null || nome == "" ||
+        indirizzo == undefined || indirizzo == null || indirizzo == ""
+    ) return false;
 
-    /*
+    let dataInput = {
+        nome: nome,
+        indirizzo: indirizzo,
+        idProprietario: uid,
+        idAffittuari: []
+    }
 
-    */
-    return true;
+    return db
+        .collection('case')
+        .add(dataInput)
+        .then( () => {
+            return true;
+        })
+        .catch((error) => {
+            console.log("ERROR!");
+            console.log(error);
+            return false;
+        });
 
 });
 
+/**
+*
+*/
+exports.partecipaCasa = functions.https.onCall((data, context) => {
+
+    const uid = context.auth.uid;
+    const uidCasa = data.idCasa;
+
+    if(
+        uidCasa == undefined || uidCasa == null || uidCasa == ""
+    ) return false;
+
+    var casaRef = db.collection("case").doc(uidCasa);
+
+    return casaRef.get()
+      .then((casaDoc) => {
+        if (casaDoc.exists) {
+            // La casa esiste
+            // Atomically add a new region to the "regions" array field.
+                casaRef.update({
+                    idAffittuari: firebase.firestore.FieldValue.arrayUnion(uid)
+                });
+                return true;
+        } else {
+            // La casa non esiste
+            return false;
+        }
+    });
+
+});
