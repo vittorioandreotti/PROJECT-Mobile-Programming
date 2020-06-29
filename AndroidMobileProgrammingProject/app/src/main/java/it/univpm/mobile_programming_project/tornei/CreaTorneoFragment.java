f701478e1d5dedc432d9f08cb0a836e36b0b515c
Vittorio Andreotti <andreottivittorio@gmail.com>
2020-06-29 20:25:19 +0200

    Implementato il DatePickerDialog per Tornei (aggiustato)

--- AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/tornei/CreaTorneoFragment.java
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/tornei/CreaTorneoFragment.java
@@ -1,6 +1,7 @@
 package it.univpm.mobile_programming_project.tornei;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
@@ -11,35 +12,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import it.univpm.mobile_programming_project.R;
 import it.univpm.mobile_programming_project.utils.picker.DatePickerFragment;
+import it.univpm.mobile_programming_project.utils.picker.TimePickerFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CreaTorneoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CreaTorneoFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+public class CreaTorneoFragment extends Fragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
-    private TextInputEditText textInputEditText;
+    private TextInputEditText tietDate;
+    private TextInputEditText tietTime;
 
     public CreaTorneoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreaTorneoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CreaTorneoFragment newInstance(String param1, String param2) {
         CreaTorneoFragment fragment = new CreaTorneoFragment();
 
@@ -60,13 +55,22 @@ public class CreaTorneoFragment extends Fragment implements DatePickerDialog.OnD
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_crea_torneo, container, false);
 
-        textInputEditText = v.findViewById(R.id.txtDataTorneo);
-        textInputEditText.setOnClickListener(new View.OnClickListener() {
+        tietDate = v.findViewById(R.id.txtDataTorneo);
+        tietDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog(v);
             }
         });
+
+        tietTime = v.findViewById(R.id.txtOraEvento);
+        tietTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v);
+            }
+        });
+
         return v;
     }
 
@@ -76,8 +80,19 @@ public class CreaTorneoFragment extends Fragment implements DatePickerDialog.OnD
         newFragment.show(fm, "datePicker");
     }
 
+    public void showTimePickerDialog (View view) {
+        DialogFragment dialogFragment = new TimePickerFragment(this);
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        dialogFragment.show(fragmentManager, "timePicker");
+    }
+
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        textInputEditText.setText(String.format("%d/%d/%d", dayOfMonth, month, year));
+        tietDate.setText(String.format("%d/%d/%d", dayOfMonth, month, year));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        tietTime.setText(String.format("%d/%d", hourOfDay, minute));
     }
 }
