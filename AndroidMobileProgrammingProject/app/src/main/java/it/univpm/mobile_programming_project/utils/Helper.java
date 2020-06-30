a2c8f3a0aa89e374b6d25756b159d4e43c518049
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-06-30 17:14:28 +0200

    Creata classe per fare il parsing della data da stringa a Date

--- AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/utils/Helper.java
+++ AndroidMobileProgrammingProject/app/src/main/java/it/univpm/mobile_programming_project/utils/Helper.java
@@ -1,5 +1,10 @@
 package it.univpm.mobile_programming_project.utils;
 
+import android.annotation.SuppressLint;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +16,21 @@ public abstract class Helper {
     public static boolean isEmailValid(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
         return matcher.find();
+    }
+
+    public static Date fromStringToDate(String dateString ) {
+        return fromStringToDate(dateString, "dd-MM-yyyy");
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    public static Date fromStringToDate(String dateString, String dateFormat ) {
+        try {
+            SimpleDateFormat dataFormat = new SimpleDateFormat(dateFormat);
+            return dataFormat.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
