b5bbdbffd56e07fd0e9c3cf9d5fa14aa923aa933
Utente <bocci.matteo98@gmail.com>
2020-07-08 18:13:56 +0200

    Login e registrazione

---
+++ XamarinApp/XamarinApp/Registrazione.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinApp.ViewModels;
+
+namespace XamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registrazione : ContentPage
+    {
+        public Registrazione()
+        {
+            var vm = new RegistrazioneViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Inserire tutti i campi", "OK");
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinApp/XamarinApp/ViewModels/RegistrazioneViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Text;
+
+namespace XamarinApp.ViewModels
+{
+    class RegistrazioneViewModel : INotifyPropertyChanged
+    {
+        
+        public Action DisplayInvalidLoginPrompt;
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                nome = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Nome"));
+            }
+        }
+
+        private string cognome;
+        public string Cognome
+        {
+            get { return cognome; }
+            set
+            {
+                cognome = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Cognome"));
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+
+        private string conferma_password;
+        public string ConfermaPassword
+        {
+            get { return conferma_password; }
+            set
+            {
+                conferma_password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("ConfermaPassword"));
+            }
+        }
+        public ICommand SubmitCommand { protected set; get; }
+        public RegistrazioneViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+        public void OnSubmit()
+        {
+            if (nome.Length == 0 || cognome.Length == 0 || email.Length == 0 || password.Length == 0 || conferma_password.Length == 0)
+            {
+                DisplayInvalidLoginPrompt();
+                return;
+            }
+
+            // Azione di registrazione con firebase
+        }
+    }
+}
