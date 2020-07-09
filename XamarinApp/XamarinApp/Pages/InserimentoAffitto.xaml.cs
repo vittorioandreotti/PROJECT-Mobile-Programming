4ef53a73708d295be30a49cadc9a9a20470884cb
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-07-09 02:40:56 +0200

    Creata Page e ViewModel per l' Inserimento dell'affitto. (DA PROVARE)

---
+++ XamarinApp/XamarinApp/Pages/InserimentoAffitto.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinApp.ViewModels;
+
+namespace XamarinApp
+{
+    public partial class InserimentoAffitto: ContentPage
+    {
+        InserimentoAffittoViewModel viewModel;
+
+        public InserimentoAffitto()
+        {
+            InitializeComponent();
+            viewModel = new InserimentoAffittoViewModel();
+            viewModel.DisplayInserisciTuttiICampi = DisplayInserisciTuttiICampi;
+            BindingContext = viewModel;
+        }
+
+        async void DisplayInserisciTuttiICampi(Spesa spesa)
+        {
+            await DisplayAlert("DATI", spesa.GetTitolo(), "OK");
+            await DisplayAlert("Errore", "Inserisci tutti i campi per continuare", "CHIUDI");
+        }
+    }
+}
