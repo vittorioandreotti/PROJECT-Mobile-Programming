493b58da97cb7fbd41422a6af42edde6fe4dc615
Utente <bocci.matteo98@gmail.com>
2020-07-09 11:41:46 +0200

    Inserimento codice casa

---
+++ XamarinApp/XamarinApp/Pages/InserisciCodiceCasa.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinApp.ViewModels;
+
+namespace XamarinApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InserisciCodiceCasa : ContentPage
+    {
+        public InserisciCodiceCasa()
+        {
+            var vm = new InserisciCodiceCasaViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidCodiceCasaPrompt += () => DisplayAlert("Error", "Inserire il codice della casa corretto", "OK");
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinApp/XamarinApp/ViewModels/InserisciCodiceCasaViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Text;
+namespace XamarinApp.ViewModels
+{
+    class InserisciCodiceCasaViewModel : INotifyPropertyChanged
+    {
+        public Action DisplayInvalidCodiceCasaPrompt;
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private string codice_casa;
+        public string CodiceCasa
+        {
+            get { return codice_casa; }
+            set
+            {
+                codice_casa = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("CodiceCasa"));
+            }
+        }
+        
+        public ICommand SubmitCommand { protected set; get; }
+        public InserisciCodiceCasaViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+        public void OnSubmit()
+        {
+            if (codice_casa.Length == 0 )
+            {
+                DisplayInvalidCodiceCasaPrompt();
+                return;
+            }
+
+            // Azione di partecipazione alla casa con firebase
+        }
+    }
+}
