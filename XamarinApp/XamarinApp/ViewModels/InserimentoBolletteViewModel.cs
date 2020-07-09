b58509f4253253cac835fba86d75516858c83f8a
Utente <bocci.matteo98@gmail.com>
2020-07-09 10:23:16 +0200

    Inserimento Bollette

---
+++ XamarinApp/XamarinApp/ViewModels/InserimentoBolletteViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinApp.ViewModels
+{
+    class InserimentoBolletteViewModel : INotifyPropertyChanged
+    {
+        public Action<Spesa> DisplayInserisciTuttiICampi;
+        public ICommand SubmitCommand { protected set; get; }
+
+        public Spesa _SpesaModel;
+        public Spesa SpesaModel
+        {
+            get
+            {
+                return _SpesaModel;
+            }
+            set
+            {
+                _SpesaModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public InserimentoBolletteViewModel()
+        {
+            SubmitCommand = new Command(InserisciBollette);
+            SpesaModel = new Spesa();
+        }
+
+        private void InserisciBollette()
+        {
+            DisplayInserisciTuttiICampi(SpesaModel);
+            // if( DataScadenza )
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
