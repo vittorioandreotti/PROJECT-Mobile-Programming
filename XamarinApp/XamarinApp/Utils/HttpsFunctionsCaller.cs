36614b4137e46a67da54d9269bcea71b5ac220a4
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-07-14 14:54:48 +0200

    Creata la classe per l'interazione https (DA FINIRE)

---
+++ XamarinApp/XamarinApp/Utils/HttpsFunctionsCaller.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace XamarinApp.Utils
+{
+    public class HttpsFunctionsCaller
+    {
+        private HttpWebRequest httpWebRequest;
+
+        private const string FIREBASE_CLOUD_FUNCTIONS_URL = "https://us-central1-programmazione-mobile.cloudfunctions.net/";
+
+        private string JsonRequest = "{}";
+
+        HttpsFunctionsCaller( string CloudFunctionName )
+        {
+            string BearerToken = "";
+
+            httpWebRequest = (HttpWebRequest)WebRequest.Create(FIREBASE_CLOUD_FUNCTIONS_URL + CloudFunctionName);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Headers.Set("Authorization", "Bearer " + BearerToken);
+            httpWebRequest.Method = "POST";
+        }
+
+        private void SetData(Dictionary<string, object> inputData)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string JsonRichiesta = JsonConvert.SerializeObject(inputData);
+                this.JsonRequest = JsonRichiesta;
+            }
+        }
+
+        public Task<Dictionary<string, Object>> Call(Dictionary<string, Object> inputData)
+        {
+            this.SetData(inputData);
+            return this.Call();
+        }
+
+        public Task<Dictionary<string, Object>> Call()
+        {
+            string BodyRequest = "{ \"data\": " + this.JsonRequest + " }";
+            return
+                (Task<Dictionary<string, Object>>) httpWebRequest
+                    .GetResponseAsync()
+                    .ContinueWith( this.HandleResponse );
+        }
+
+        private Dictionary<string, Object> HandleResponse(Task<WebResponse> response)
+        {
+            WebResponse webResponse = response.Result;
+
+            //Dictionary<string, Object> ResponseData;
+
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+
+            Console.WriteLine()
+
+
+            return new Dictionary<string, Object>();
+
+            throw new NotImplementedException();
+
+            private Dictionary<string, Object> HandleResponse(Task<Dictionary<string, Object>> response)
+            {
+
+                return;
+            }
+
+        }
+    }
+}
