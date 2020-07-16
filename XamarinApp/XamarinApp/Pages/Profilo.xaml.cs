f788e11d7157a6e3f0b44375125c5d311b59b475
Vittorio Andreotti <andreottivittorio@gmail.com>
2020-07-16 12:30:38 +0200

    Bollette

--- XamarinApp/XamarinApp/Pages/Profilo.xaml.cs
+++ XamarinApp/XamarinApp/Pages/Profilo.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinApp.Utils;
 using XamarinApp.ViewModels;
 
 namespace XamarinApp.Pages
@@ -12,9 +13,13 @@ namespace XamarinApp.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Profilo : ContentPage
     {
+        UtentePreferences utentePreferences = new UtentePreferences();
         public Profilo()
         {
             InitializeComponent();
+            Nome.Text = utentePreferences.GetNome();
+            Cognome.Text = utentePreferences.GetCognome();
+            Email.Text = utentePreferences.GetEmail();
         }
 
         private void ModificaPassword(object sender, EventArgs e)
---
+++ XamarinApp/XamarinApp/ViewModels/BolletteViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinApp.Models.Helpers;
+using XamarinApp.Utils;
+
+namespace XamarinApp.ViewModels
+{
+    public class BolletteViewModel
+    {
+        public IList<Spesa> CardData { get; set; }
+        UtentePreferences utentePreferences;
+        public BolletteViewModel ()
+        {
+            utentePreferences = new UtentePreferences();
+            Dictionary<string, object> inputData = new Dictionary<string, object>();
+            HttpsFunctionsCaller httpsFunctionsCaller = new HttpsFunctionsCaller("elencoSpese");
+            inputData.Add("idCasa", utentePreferences.GetIdCasa());
+            httpsFunctionsCaller.Call(inputData).ContinueWith(GenerateBolletteModel);
+        }
+
+        private void GenerateBolletteModel(Task<CloudFunctionResponse> taskResponse)
+        {
+            taskResponse.Wait();
+            CloudFunctionResponse elencoSpese = taskResponse.Result;
+            CardData = new ObservableCollection<Spesa>();
+            foreach (JObject spesa in (JArray)elencoSpese.JsonData["spese"])
+            {
+                if(((string)spesa["tipo"]).Equals("bolletta"))
+                {
+                    CardData.Add(new Spesa
+                    {
+                        Categoria = (string)spesa["categoria"],
+                        Prezzo = (double)spesa["prezzo"],
+                        DataInserimento = Helper.FromMillisToDate((long)spesa["dataInserimento"]),
+                        DataScadenza = Helper.FromMillisToDate((long)spesa["dataScadenza"]),
+                        DataPagamento = Helper.FromMillisToDate((long)spesa["dataPagamento"]),
+                    });
+                }
+            }
+        }
+    }
+}
